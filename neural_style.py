d6f84a17a007b552c9b79a44b161b5cad22168f2
Kyle Cui <kcui@kyles-mbp-3.devices.brown.edu>
2019-05-10 14:49:33 -0400

    abstracted out vgg19

--- neural_style.py
+++ neural_style.py
@@ -7,10 +7,10 @@ import errno
 import time                       
 import cv2
 import os
-
+import vgg19
 
 # Weights + biases of VGG-19 network
-model_weights = 'imagenet-vgg-verydeep-19.mat'
+vgg_path = '../imagenet-vgg-verydeep-19.mat'
 # Weight for content loss
 content_weight = 5e0
 # Interpolation weight of the style image(s)
@@ -35,9 +35,6 @@ max_iterations = 1000
 
 # Indices of previous frames to be considered for long-term temporal consistency
 prev_frame_indices = [1]
-
-
-
 
 '''
   parsing and configuration
@@ -91,112 +88,6 @@ def parse_args():
     maybe_make_directory('./image_output')
 
   return args
-
-'''
-  pre-trained vgg19 convolutional neural network
-  remark: layers are manually initialized for clarity.
-'''
-
-def build_model(input_img):
-  print('\nBUILDING VGG-19 NETWORK')
-  net = {}
-  _, h, w, d     = input_img.shape
-  
-  print('loading model weights...')
-  vgg_rawnet     = scipy.io.loadmat(model_weights)
-  vgg_layers     = vgg_rawnet['layers'][0]
-  print('constructing layers...')
-  net['input']   = tf.Variable(np.zeros((1, h, w, d), dtype=np.float32))
-
-  print('LAYER GROUP 1')
-  net['conv1_1'] = conv_layer('conv1_1', net['input'], W=get_weights(vgg_layers, 0))
-  net['relu1_1'] = relu_layer('relu1_1', net['conv1_1'], b=get_bias(vgg_layers, 0))
-
-  net['conv1_2'] = conv_layer('conv1_2', net['relu1_1'], W=get_weights(vgg_layers, 2))
-  net['relu1_2'] = relu_layer('relu1_2', net['conv1_2'], b=get_bias(vgg_layers, 2))
-  
-  net['pool1']   = pool_layer('pool1', net['relu1_2'])
-
-  print('LAYER GROUP 2')  
-  net['conv2_1'] = conv_layer('conv2_1', net['pool1'], W=get_weights(vgg_layers, 5))
-  net['relu2_1'] = relu_layer('relu2_1', net['conv2_1'], b=get_bias(vgg_layers, 5))
-  
-  net['conv2_2'] = conv_layer('conv2_2', net['relu2_1'], W=get_weights(vgg_layers, 7))
-  net['relu2_2'] = relu_layer('relu2_2', net['conv2_2'], b=get_bias(vgg_layers, 7))
-  
-  net['pool2']   = pool_layer('pool2', net['relu2_2'])
-  
-  print('LAYER GROUP 3')
-  net['conv3_1'] = conv_layer('conv3_1', net['pool2'], W=get_weights(vgg_layers, 10))
-  net['relu3_1'] = relu_layer('relu3_1', net['conv3_1'], b=get_bias(vgg_layers, 10))
-
-  net['conv3_2'] = conv_layer('conv3_2', net['relu3_1'], W=get_weights(vgg_layers, 12))
-  net['relu3_2'] = relu_layer('relu3_2', net['conv3_2'], b=get_bias(vgg_layers, 12))
-
-  net['conv3_3'] = conv_layer('conv3_3', net['relu3_2'], W=get_weights(vgg_layers, 14))
-  net['relu3_3'] = relu_layer('relu3_3', net['conv3_3'], b=get_bias(vgg_layers, 14))
-
-  net['conv3_4'] = conv_layer('conv3_4', net['relu3_3'], W=get_weights(vgg_layers, 16))
-  net['relu3_4'] = relu_layer('relu3_4', net['conv3_4'], b=get_bias(vgg_layers, 16))
-
-  net['pool3']   = pool_layer('pool3', net['relu3_4'])
-
-  print('LAYER GROUP 4')
-  net['conv4_1'] = conv_layer('conv4_1', net['pool3'], W=get_weights(vgg_layers, 19))
-  net['relu4_1'] = relu_layer('relu4_1', net['conv4_1'], b=get_bias(vgg_layers, 19))
-
-  net['conv4_2'] = conv_layer('conv4_2', net['relu4_1'], W=get_weights(vgg_layers, 21))
-  net['relu4_2'] = relu_layer('relu4_2', net['conv4_2'], b=get_bias(vgg_layers, 21))
-
-  net['conv4_3'] = conv_layer('conv4_3', net['relu4_2'], W=get_weights(vgg_layers, 23))
-  net['relu4_3'] = relu_layer('relu4_3', net['conv4_3'], b=get_bias(vgg_layers, 23))
-
-  net['conv4_4'] = conv_layer('conv4_4', net['relu4_3'], W=get_weights(vgg_layers, 25))
-  net['relu4_4'] = relu_layer('relu4_4', net['conv4_4'], b=get_bias(vgg_layers, 25))
-
-  net['pool4']   = pool_layer('pool4', net['relu4_4'])
-
-  print('LAYER GROUP 5')
-  net['conv5_1'] = conv_layer('conv5_1', net['pool4'], W=get_weights(vgg_layers, 28))
-  net['relu5_1'] = relu_layer('relu5_1', net['conv5_1'], b=get_bias(vgg_layers, 28))
-
-  net['conv5_2'] = conv_layer('conv5_2', net['relu5_1'], W=get_weights(vgg_layers, 30))
-  net['relu5_2'] = relu_layer('relu5_2', net['conv5_2'], b=get_bias(vgg_layers, 30))
-
-  net['conv5_3'] = conv_layer('conv5_3', net['relu5_2'], W=get_weights(vgg_layers, 32))
-  net['relu5_3'] = relu_layer('relu5_3', net['conv5_3'], b=get_bias(vgg_layers, 32))
-
-  net['conv5_4'] = conv_layer('conv5_4', net['relu5_3'], W=get_weights(vgg_layers, 34))
-  net['relu5_4'] = relu_layer('relu5_4', net['conv5_4'], b=get_bias(vgg_layers, 34))
-
-  net['pool5']   = pool_layer('pool5', net['relu5_4'])
-
-  return net
-
-def conv_layer(layer_name, layer_input, W):
-  conv = tf.nn.conv2d(layer_input, W, strides=[1, 1, 1, 1], padding='SAME')
-  print('--{} | shape={} | weights_shape={}'.format(layer_name, conv.get_shape(), W.get_shape()))
-  return conv
-
-def relu_layer(layer_name, layer_input, b):
-  relu = tf.nn.relu(layer_input + b)
-  print('--{} | shape={} | bias_shape={}'.format(layer_name, relu.get_shape(), b.get_shape()))
-  return relu
-
-def pool_layer(layer_name, layer_input):
-  pool = tf.nn.avg_pool(layer_input, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-  print('--{}   | shape={}'.format(layer_name, pool.get_shape()))
-  return pool
-
-def get_weights(vgg_layers, i):
-  weights = vgg_layers[i][0][0][2][0][0]
-  W = tf.constant(weights)
-  return W
-
-def get_bias(vgg_layers, i):
-  bias = vgg_layers[i][0][0][2][0][1]
-  b = tf.constant(np.reshape(bias, (bias.size)))
-  return b
 
 '''
   'a neural algorithm for artistic style' loss functions
@@ -369,7 +260,8 @@ def check_image(img, path):
 def stylize(content_img, style_imgs, init_img, frame=None):
   with tf.device(args.device), tf.Session() as sess:
     # setup network
-    net = build_model(content_img)
+    vgg = vgg19.VGG19(content_img, vgg_path=vgg_path)
+    net = vgg.get_model()
     
     # style loss
     L_style = sum_style_losses(sess, net, style_imgs)
---
+++ vgg19.py
@@ -0,0 +1,115 @@
+import tensorflow as tf
+import numpy as np
+import scipy.io
+import time
+
+class VGG19:
+  def __init__(self, img, vgg_path = 'imagenet-vgg-verydeep-19.mat'):
+    print("loading vgg file...")
+    vgg_rawnet = scipy.io.loadmat(vgg_path)
+    self.vgg_layers = vgg_rawnet['layers'][0]
+    print("vgg file loaded.")
+    print("building model...")
+    start = time.time()
+    self.model = self.build_model(img)
+    end = time.time()
+    print("build model finished. time: %ds" % (end - start))
+
+  def build_model(self, input_img):
+    graph = {}
+    _, h, w, d     = input_img.shape
+    print('constructing layers...')
+    graph['input']   = tf.Variable(np.zeros((1, h, w, d), dtype=np.float32))
+
+    print('LAYER GROUP 1')
+    graph['conv1_1'] = self.conv_layer('conv1_1', graph['input'], W=self.get_weights(self.vgg_layers, 0))
+    graph['relu1_1'] = self.relu_layer('relu1_1', graph['conv1_1'], b=self.get_bias(self.vgg_layers, 0))
+
+    graph['conv1_2'] = self.conv_layer('conv1_2', graph['relu1_1'], W=self.get_weights(self.vgg_layers, 2))
+    graph['relu1_2'] = self.relu_layer('relu1_2', graph['conv1_2'], b=self.get_bias(self.vgg_layers, 2))
+    
+    graph['pool1']   = self.pool_layer('pool1', graph['relu1_2'])
+
+    print('LAYER GROUP 2')  
+    graph['conv2_1'] = self.conv_layer('conv2_1', graph['pool1'], W=self.get_weights(self.vgg_layers, 5))
+    graph['relu2_1'] = self.relu_layer('relu2_1', graph['conv2_1'], b=self.get_bias(self.vgg_layers, 5))
+    
+    graph['conv2_2'] = self.conv_layer('conv2_2', graph['relu2_1'], W=self.get_weights(self.vgg_layers, 7))
+    graph['relu2_2'] = self.relu_layer('relu2_2', graph['conv2_2'], b=self.get_bias(self.vgg_layers, 7))
+    
+    graph['pool2']   = self.pool_layer('pool2', graph['relu2_2'])
+    
+    print('LAYER GROUP 3')
+    graph['conv3_1'] = self.conv_layer('conv3_1', graph['pool2'], W=self.get_weights(self.vgg_layers, 10))
+    graph['relu3_1'] = self.relu_layer('relu3_1', graph['conv3_1'], b=self.get_bias(self.vgg_layers, 10))
+
+    graph['conv3_2'] = self.conv_layer('conv3_2', graph['relu3_1'], W=self.get_weights(self.vgg_layers, 12))
+    graph['relu3_2'] = self.relu_layer('relu3_2', graph['conv3_2'], b=self.get_bias(self.vgg_layers, 12))
+
+    graph['conv3_3'] = self.conv_layer('conv3_3', graph['relu3_2'], W=self.get_weights(self.vgg_layers, 14))
+    graph['relu3_3'] = self.relu_layer('relu3_3', graph['conv3_3'], b=self.get_bias(self.vgg_layers, 14))
+
+    graph['conv3_4'] = self.conv_layer('conv3_4', graph['relu3_3'], W=self.get_weights(self.vgg_layers, 16))
+    graph['relu3_4'] = self.relu_layer('relu3_4', graph['conv3_4'], b=self.get_bias(self.vgg_layers, 16))
+
+    graph['pool3']   = self.pool_layer('pool3', graph['relu3_4'])
+
+    print('LAYER GROUP 4')
+    graph['conv4_1'] = self.conv_layer('conv4_1', graph['pool3'], W=self.get_weights(self.vgg_layers, 19))
+    graph['relu4_1'] = self.relu_layer('relu4_1', graph['conv4_1'], b=self.get_bias(self.vgg_layers, 19))
+
+    graph['conv4_2'] = self.conv_layer('conv4_2', graph['relu4_1'], W=self.get_weights(self.vgg_layers, 21))
+    graph['relu4_2'] = self.relu_layer('relu4_2', graph['conv4_2'], b=self.get_bias(self.vgg_layers, 21))
+
+    graph['conv4_3'] = self.conv_layer('conv4_3', graph['relu4_2'], W=self.get_weights(self.vgg_layers, 23))
+    graph['relu4_3'] = self.relu_layer('relu4_3', graph['conv4_3'], b=self.get_bias(self.vgg_layers, 23))
+
+    graph['conv4_4'] = self.conv_layer('conv4_4', graph['relu4_3'], W=self.get_weights(self.vgg_layers, 25))
+    graph['relu4_4'] = self.relu_layer('relu4_4', graph['conv4_4'], b=self.get_bias(self.vgg_layers, 25))
+
+    graph['pool4']   = self.pool_layer('pool4', graph['relu4_4'])
+
+    print('LAYER GROUP 5')
+    graph['conv5_1'] = self.conv_layer('conv5_1', graph['pool4'], W=self.get_weights(self.vgg_layers, 28))
+    graph['relu5_1'] = self.relu_layer('relu5_1', graph['conv5_1'], b=self.get_bias(self.vgg_layers, 28))
+
+    graph['conv5_2'] = self.conv_layer('conv5_2', graph['relu5_1'], W=self.get_weights(self.vgg_layers, 30))
+    graph['relu5_2'] = self.relu_layer('relu5_2', graph['conv5_2'], b=self.get_bias(self.vgg_layers, 30))
+
+    graph['conv5_3'] = self.conv_layer('conv5_3', graph['relu5_2'], W=self.get_weights(self.vgg_layers, 32))
+    graph['relu5_3'] = self.relu_layer('relu5_3', graph['conv5_3'], b=self.get_bias(self.vgg_layers, 32))
+
+    graph['conv5_4'] = self.conv_layer('conv5_4', graph['relu5_3'], W=self.get_weights(self.vgg_layers, 34))
+    graph['relu5_4'] = self.relu_layer('relu5_4', graph['conv5_4'], b=self.get_bias(self.vgg_layers, 34))
+
+    graph['pool5']   = self.pool_layer('pool5', graph['relu5_4'])
+
+    return graph
+
+  def conv_layer(self, layer_name, layer_input, W):
+    conv = tf.nn.conv2d(layer_input, W, strides=[1, 1, 1, 1], padding='SAME')
+    print('--{} | shape={} | weights_shape={}'.format(layer_name, conv.get_shape(), W.get_shape()))
+    return conv
+
+  def relu_layer(self, layer_name, layer_input, b):
+    relu = tf.nn.relu(layer_input + b)
+    print('--{} | shape={} | bias_shape={}'.format(layer_name, relu.get_shape(), b.get_shape()))
+    return relu
+
+  def pool_layer(self, layer_name, layer_input):
+    pool = tf.nn.avg_pool(layer_input, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+    print('--{}   | shape={}'.format(layer_name, pool.get_shape()))
+    return pool
+
+  def get_weights(self, vgg_layers, i):
+    weights = self.vgg_layers[i][0][0][2][0][0]
+    W = tf.constant(weights)
+    return W
+
+  def get_bias(self, vgg_layers, i):
+    bias = self.vgg_layers[i][0][0][2][0][1]
+    b = tf.constant(np.reshape(bias, (bias.size)))
+    return b
+    
+  def get_model(self):
+    return self.model
